409e438d797212026e350f855ba9101efb902724
makssoccer <82469846+makssoccer@users.noreply.github.com>
2023-01-24 21:37:56 +0300

    o

--- ATM/src/main/java/org/example/service/CardService.java
+++
@@ -1,9 +0,0 @@
-package org.example.service;
-
-import org.example.entity.Card;
-
-public interface CardService {
-    public void addCard(Card card);
-
-    public Card authorization (String numberCard);
-}
